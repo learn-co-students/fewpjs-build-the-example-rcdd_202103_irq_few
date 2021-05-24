77fa85a02a7053a969f7745340503e25898731f2
Omer Sabah <omerrj99@gmail.com>
2021-05-24 12:46:10 +0000

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const glyphs=document.getElementsByClassName('like-glyph');
+const like=(e) =>{
+  const heart = e.target;
+  mimicServerCall("url")
+    .then(()=>{
+      if(heart.innerText===EMPTY_HEART){
+        heart.innerText=FULL_HEART
+        heart.classList.add('activated-heart')
+        
+      }else{
+        heart.innerText=EMPTY_HEART
+        heart.classList.remove('activated-heart')
+      }
+    })
+    .catch(e =>{
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = e;
+      setTimeout(() =>  modal.className = "hidden", 2000);
+    });
+}
+
+[...glyphs].forEach((glyph)=>{
+  glyph.onclick=(e)=>like(e);
+  
+})
 
 
 
