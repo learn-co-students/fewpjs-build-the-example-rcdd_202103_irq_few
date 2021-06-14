75631d39d57b4f104e89d7c1e21cb4663d858a79
Daban Sadiq <dabansadiq@icloud.com>
2021-06-14 14:18:18 +0000

    Done.

--- main.js
+++ main.js
@@ -1,25 +1,58 @@
-// Defining text characters for the empty and full hearts for you to use later.
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('zug zug')
+})
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
+const likeButtons = document.querySelectorAll('.like')
 
-// Your JavaScript code goes here!
+errorModal.className = 'hidden'
 
+likeButtons.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    mimicServerCall()
+      .then(function (response) {
+        console.log(response)
+        likeHandler(button)
+      })
+      .catch(function (error) {
+        errorHandler(error)
+      })
+  })
+})
 
+function likeHandler(button) {
+  if (button.innerHTML.includes('Like!')) {
+    button.innerText = `Unlike! ${FULL_HEART}`
+    button.className += ' activated-heart' // Is there a better way to do this?
+  } else {
+    button.innerText = `Like! ${EMPTY_HEART}`
+    button.classList.remove('activated-heart')
+  }
+}
 
+function errorHandler(error) {
+  errorModal.innerHTML = `<h2>Error!</h2><p id="modal-message">${error}</p>`
+  errorModal.className = 'visible'
+  setTimeout(function () {
+    errorModal.className = 'hidden'
+  }, 5000)
+}
 
 //------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
+// Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject('Random server error. Try again.')
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve('Pretend remote server notified of action!')
       }
-    }, 300);
-  });
+    }, 300)
+  })
 }
