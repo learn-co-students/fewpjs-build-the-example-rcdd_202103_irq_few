dfedd91d0a8a15846bf5ef6ae6fb4cef86c6bffd
omar ammaj <oamaje@gmail.com>
2021-06-23 18:58:59 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function likeCallback(e) {
     })
     .catch(function(error) {
       const modal = document.getElementById("modal");
-      modal.className = "";
+      modal.className = "hidden";
       modal.innerText = error;
       setTimeout(() =>  modal.className = "hidden", 3000);
     });
